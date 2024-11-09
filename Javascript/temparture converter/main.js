67902a0b8abdacbcb0dc4f1109aeab3d97955e9b
Rohan Janrao <janraorohan2@gmail.com>
2024-11-09 20:37:31 +0530

    temprature completed

--- Javascript/temparture converter/main.js
+++ Javascript/temparture converter/main.js
@@ -1,33 +1,58 @@
-  currentValue = document.getElementById("temperatureInput");
-  tempType = document.getElementById("tempType");
-  convertTo = document.getElementById("temptoConvertIn");
+// function celsiusToFahrenheit(temp) {
+//   return (temp * 9.0) / 5.0 + 32.0;
+// }
 
-  function celsiusToFahrenheit(temp) {
-    return (temp * 9.0) / 5.0 + 32.0;
+// function fahrenheitToCelsius(temp) {
+//   return ((temp - 32) * 5) / 9;
+// }
+
+// function convertTemp(result) {
+//   if (
+//     (tempType.value == "fahrenheit" && convertTo.value == "fahrenheit") ||
+//     (tempType.value == "celsius" && convertTo.value == "celsius")
+//   ) {
+//     return (document.getElementById("tempoutput").innerText =
+//       currentValue.value);
+//   }
+//   else if (tempType.value == "fahrenheit" && convertTo.value == "celsius")
+//   {
+//     return document.getElementById("tempoutput").innerText = parseInt(
+//       fahrenheitToCelsius(currentValue.value)
+//     );
+//   } else if (tempType.value == "celsius" && convertTo.value == "fahrenheit")
+//   {
+//     return (document.getElementById("tempoutput").innerText = parseInt(
+//       celsiusToFahrenheit(currentValue.value)
+//     ));
+//   }
+// }
+
+function convertTemperature() {
+  const currentValue = document.getElementById("temperatureInput").value;
+  const tempType = document.getElementById("tempType").value;
+  const convertTo = document.getElementById("temptoConvertIn").value;
+  let result = 0;
+
+
+  console.log(currentValue);
+  console.log(tempType);
+  console.log(convertTo);
+
+
+  if (tempType == "celsius" && convertTo == "F") {
+    result = (currentValue * 9) / 5 + 32;
+    // return console.log(result);
+  } else if (tempType == "fahrenheit" && convertTo == "C") {
+   (result = ((currentValue - 32) * 5) / 9);
+  //  return console.log(result);
+
+  } else {
+    result = currentValue;
+    //  console.log(result);
+
+
+    
   }
 
-  function fahrenheitToCelsius(temp) {
-    return ((temp - 32) * 5) / 9;
-  }
-
-  function convertTemp(result) {
-    if (
-      (tempType.value == "fahrenheit" && convertTo.value == "fahrenheit") ||
-      (tempType.value == "celsius" && convertTo.value == "celsius")
-    ) {
-      return (document.getElementById("tempoutput").innerText =
-        currentValue.value);
-    } 
-    else if (tempType.value == "fahrenheit" && convertTo.value == "celsius")
-    {
-      return document.getElementById("tempoutput").innerText = parseInt(
-        fahrenheitToCelsius(currentValue.value)
-      );
-    } else if (tempType.value == "celsius" && convertTo.value == "fahrenheit")
-    {
-      return (document.getElementById("tempoutput").innerText = parseInt(
-        celsiusToFahrenheit(currentValue.value)
-      ));
-    }
-  }
-
+  document.getElementById("tempoutput").innerText = `${parseInt(result)}°${convertTo}`;
+}
