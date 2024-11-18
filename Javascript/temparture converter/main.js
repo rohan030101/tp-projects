350c365eb69032694181c1c3d5091a3942eb52c9
Rohan Janrao <janraorohan2@gmail.com>
2024-11-18 20:46:37 +0530

    git mentor review pending

--- Javascript/temparture converter/main.js
+++ Javascript/temparture converter/main.js
@@ -1,58 +1,26 @@
-// function celsiusToFahrenheit(temp) {
-//   return (temp * 9.0) / 5.0 + 32.0;
-// }
-
-// function fahrenheitToCelsius(temp) {
-//   return ((temp - 32) * 5) / 9;
-// }
-
-// function convertTemp(result) {
-//   if (
-//     (tempType.value == "fahrenheit" && convertTo.value == "fahrenheit") ||
-//     (tempType.value == "celsius" && convertTo.value == "celsius")
-//   ) {
-//     return (document.getElementById("tempoutput").innerText =
-//       currentValue.value);
-//   }
-//   else if (tempType.value == "fahrenheit" && convertTo.value == "celsius")
-//   {
-//     return document.getElementById("tempoutput").innerText = parseInt(
-//       fahrenheitToCelsius(currentValue.value)
-//     );
-//   } else if (tempType.value == "celsius" && convertTo.value == "fahrenheit")
-//   {
-//     return (document.getElementById("tempoutput").innerText = parseInt(
-//       celsiusToFahrenheit(currentValue.value)
-//     ));
-//   }
-// }
-
 function convertTemperature() {
-  const currentValue = document.getElementById("temperatureInput").value;
-  const tempType = document.getElementById("tempType").value;
-  const convertTo = document.getElementById("temptoConvertIn").value;
+  let currentValue = document.getElementById("temperatureInput").value;
+  let tempType = document.getElementById("tempType");
+  let convertTo = document.getElementById("temptoConvertIn").value;
   let result = 0;
-
 
   console.log(currentValue);
   console.log(tempType);
   console.log(convertTo);
 
-
-  if (tempType == "celsius" && convertTo == "F") {
+  if (tempType.value == "celsius" && convertTo == "F") {
     result = (currentValue * 9) / 5 + 32;
-    // return console.log(result);
-  } else if (tempType == "fahrenheit" && convertTo == "C") {
-   (result = ((currentValue - 32) * 5) / 9);
-  //  return console.log(result);
-
+  } else if (tempType.value == "fahrenheit" && convertTo == "C") {
+    result = ((currentValue - 32) * 5) / 9;
+  } else if (tempType.value == "fahrenheit" && convertTo && "F") {
+    tempType.value = "celsius";
+    result = (currentValue * 9) / 5 + 32;
   } else {
-    result = currentValue;
-    //  console.log(result);
-
-
-    
+    tempType.value = "fahrenheit";
+    result = ((currentValue - 32) * 5) / 9;
   }
 
-  document.getElementById("tempoutput").innerText = `${parseInt(result)}°${convertTo}`;
+  document.getElementById("tempoutput").innerHTML = `${parseFloat(
+    result
+  ).toFixed(2)}<sup>°${convertTo}</sup>`;
 }
